5d4c0d3ca0dc3d226c19f63bf45a6cd217fd292c
thienlong <thienlong@localhost>
2015-03-25 15:05:28 +0700

    improve localThreshold api

--- include/uvproc.h
+++ include/uvproc.h
@@ -10,6 +10,49 @@
 #include "opencv2/core/core.hpp"
 #include <vector>
 
+template<class T> class average {
+private:
+	std::vector<T> values;
+	T sum;
+public:
+	average() : sum(0) {}
+	void update(T value) {
+		values.push_back(value);
+		sum += value;
+	}
+
+	double mean() {
+		return sum /(double) values.size();
+	}
+
+	T max() {
+		auto iter = std::max_element(values.begin(), values.end());
+		return *iter;
+	}
+
+	double deviation() {
+		double m = mean();
+		double devSum(0);
+		for (T v : values) {
+			devSum += abs(m - v);
+		}
+		return devSum / values.size();
+	}
+
+	double sdeviation() {
+		double m = mean();
+		double devSum(0);
+		for (T v : values) {
+			devSum += (m - v) * (m - v);
+		}
+		return sqrt(devSum / (double) values.size());
+	}
+
+	int size() {
+		return values.size();
+	}
+};
+
 void adjustAutoLevels(cv::Mat& src, cv::Mat& dst);
 void drawHist(cv::Mat& src);
 void projectVertical(cv::Mat &input, std::vector<int> &output);
--- src/UVAnalyzer.cpp
+++ src/UVAnalyzer.cpp
@@ -14,6 +14,9 @@
 using namespace cv;
 
 namespace icr {
+#define EPICK_HEIGHT 65
+#define EPICK_WIDTH 200
+#define EPICK_ON_BOTTOM 500
 
 UVAnalyzer::UVAnalyzer() {
 
@@ -33,27 +36,96 @@ bool needLocalThreshold(cv::Mat& binary) {
 	return (startX < 20);
 }
 
-void localThreshold(cv::Mat& src, cv::Mat& output) {
-	Rect roi(0, 272, src.cols, 152);
+#define MID_Y 272
+#define MID_HEIGHT 152
+double thresholdMid(cv::Mat& src, cv::Mat& output) {
+	Rect roi(0, MID_Y, src.cols, MID_HEIGHT);
 	Mat roiMid = src(roi);
-	Mat mOutput = output(roi);
-	double thres = threshold(roiMid, mOutput, 0, 255, THRESH_OTSU | THRESH_BINARY);
-	roi.y += roi.height;
-	roi.height = src.rows - roi.y;
+	Mat outputMid = output(roi);
+	double thres = threshold(roiMid, outputMid, 0, 255, THRESH_OTSU | THRESH_BINARY);
+	std::vector<int> projects;
+	projectVertical(outputMid, projects);
+	int thresX = roiMid.rows / 7;
+	int startX = 0;
+	int endX = roiMid.cols - 1;
+	for (; startX < roiMid.cols && projects[startX] < thresX; ++startX) {}
+	for (; endX >= 0 && projects[endX] < thresX; --endX) {}
+	if (startX > 20 || roiMid.cols - endX > 25) {
+		return thres;
+	}
+	std::cout << "Problem" << std::endl;
+	average<int> avg;
+	for (int r = 0; r < roiMid.rows; ++r) {
+		for (int c = 0; c <= 20; ++c) {
+			if (outputMid.at<uchar>(r,c) > 0) {
+				avg.update(roiMid.at<uchar>(r,c));
+			}
+		}
+	}
+	for (int r = 0; r < roiMid.rows; ++r) {
+		for (int c = roiMid.cols - 25; c < roiMid.cols; ++c) {
+			if (outputMid.at<uchar>(r, c) > 0) {
+				avg.update(roiMid.at<uchar>(r, c));
+			}
+		}
+	}
+	thres = avg.sdeviation() + avg.mean() + avg.deviation();
+	threshold(roiMid, outputMid, thres, 255, THRESH_BINARY);
+	return thres;
+}
+
+double thresholdTop(cv::Mat& src, cv::Mat& output) {
+	Rect roi(0, 0, src.cols, MID_Y);
+	Mat roiTop = src(roi);
+	Mat outputTop = output(roi);
+	double thres = threshold(roiTop, outputTop, 0, 255, THRESH_OTSU | THRESH_BINARY);
+	std::vector<int> projects;
+	projectVertical(outputTop, projects);
+	int thresX = roiTop.rows / 7;
+	int startX = 0;
+	int endX = roiTop.cols - startX;
+	for (; startX < roiTop.cols && projects[startX] < thresX; ++startX) {}
+	for (; endX >= 0 && projects[endX] < thresX; --endX) {}
+	if (startX > 20 || roiTop.cols - endX > 25) {
+		return thres;
+	}
+	std::cout << "Problem" << std::endl;
+	average<int> avg;
+	for (int r = 0; r < roiTop.rows; ++r) {
+		for (int c = 0; c <= 20; ++c) {
+			if (outputTop.at<uchar>(r,c) > 0) {
+				avg.update(roiTop.at<uchar>(r,c));
+			}
+		}
+	}
+	for (int r = 0; r < roiTop.rows; ++r) {
+		for (int c = roiTop.cols - 25; c < roiTop.cols; ++c) {
+			if (outputTop.at<uchar>(r, c) > 0) {
+				avg.update(roiTop.at<uchar>(r, c));
+			}
+		}
+	}
+	thres = avg.sdeviation() + avg.mean() + avg.deviation();
+	threshold(roiTop, outputTop, thres, 255, THRESH_BINARY);
+	return thres;
+}
+
+void localThreshold(cv::Mat& src, cv::Mat& output) {
+	double thres = thresholdMid(src, output);
+	Rect roi(0, MID_Y + MID_HEIGHT, src.cols, src.rows - MID_Y - MID_HEIGHT);
 	Mat roiBot = src(roi);
 	Mat outBot = output(roi);
 	threshold(roiBot, outBot, thres, 255, THRESH_BINARY);
-	roi.y = 0;
-	roi.height = 272;
-	Mat roiTop = src(roi);
-	Mat outTop = output(roi);
-	threshold(roiTop, outTop, 0, 255, THRESH_OTSU | THRESH_BINARY);
+	thresholdTop(src, output);
 }
 
-bool detectAreas(cv::Mat& src, vector<Rect>& rects) {
+
+
+void detectAreas(cv::Mat& src, vector<Rect>& rects) {
 	std::vector<int> projects;
+	//vertical
 	projectVertical(src, projects);
-	int thresX = src.rows / 6;
+	int thresX = src.rows / 7;
 	int startX = 10;
 	for (; startX < src.cols && projects[startX] < thresX; ++startX) {}
 	int endX = src.cols - 10;
@@ -65,14 +137,25 @@ bool detectAreas(cv::Mat& src, vector<Rect>& rects) {
 	for (; startY < src.rows && projects[startY] < thresY; ++startY) {}
 	int endY = src.rows - 10;
 	for (; endY >= 0&& projects[endY] < thresY; --endY) {}
+
+	//debug draw
 	line( src, Point(startX, 0), Point(startX, src.rows), Scalar(255,255, 255), 1, CV_AA);
 	line( src, Point(endX, 0), Point(endX, src.rows), Scalar(255,255, 255), 1, CV_AA);
 	line( src, Point(0, startY), Point(src.cols, startY), Scalar(255,255, 255), 1, CV_AA);
 	line( src, Point(0, endY), Point(src.cols, endY), Scalar(255,255, 255), 1, CV_AA);
+
+	Rect roi(startX, startY, endX - startX, endY - startY);
+	Mat temp = src(roi);
 	if (endY - startY < src.rows * 0.28 || endX - startX < src.cols * 0.8) {
-		return false;
+		throw std::bad_exception();
+	} else if (endY - startY < 4 * EPICK_HEIGHT) {
+		//form 2 epick
+		std::cout << "form 2" << std::endl;
 	}
-	return true;
+	if (endY > EPICK_ON_BOTTOM) {
+		//option epick on bottom
+		std::cout <<"epick on bottom" << std::endl;
+	}
 }
 
 float UVAnalyzer::checkValid(cv::Mat& uvImg) {
@@ -87,10 +170,8 @@ float UVAnalyzer::checkValid(cv::Mat& uvImg) {
 		localThreshold(uvImg, bi);
 	}
 	vector<Rect> rects;
-	if (!detectAreas(bi, rects)) {
-		return 0;
-	}
 	imshow("bi", bi);
+	detectAreas(bi, rects);
 	return 1;
 }
 
