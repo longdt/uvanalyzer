2043bec7a10e54d6c6142b35ccbf9e4ef661803e
longdt <longdt@localhost>
2015-03-30 14:39:18 +0700

    implement detect UV area

--- src/UVAnalyzer.cpp
+++ src/UVAnalyzer.cpp
@@ -119,7 +119,137 @@ void localThreshold(cv::Mat& src, cv::Mat& output) {
 	thresholdTop(src, output);
 }
 
+Rect findBoundingRect(cv::Mat& src, float thresholdX, float thresholdY) {
+	std::vector<int> projects;
+	//vertical
+	projectVertical(src, projects);
+	int startX = 0;
+	for (; startX < src.cols && projects[startX] < thresholdX; ++startX) {}
+	if (startX == src.cols) {
+		return Rect();
+	}
+	int endX = src.cols - 1;
+	for (; endX >= 0 && projects[endX] < thresholdX; --endX) {}
+	if (endX == -1) {
+		return Rect();
+	}
+	//horizontal
+	projectHorizontal(src, projects);
+	int startY = 0;
+	for (; startY < src.rows && projects[startY] < thresholdY; ++startY) {}
+	if (startY == src.rows) {
+		return Rect();
+	}
+	int endY = src.rows - 1;
+	for (; endY >= 0 && projects[endY] < thresholdY; --endY) {}
+	if (endY == -1) {
+		return Rect();
+	}
+	return Rect(startX, startY, endX - startX + 1, endY - startY + 1);
+}
 
+#define FORM1 1
+#define FORM2 2
+#define FORM3 3
+int detectForm(cv::Mat& src, cv::Rect& area, vector<Rect>& rects) {
+	float form2Score = area.width > 6 * EPICK_WIDTH ? 0.5 : 0;
+	Rect botRoi(area.x + area.width - 2 * EPICK_WIDTH, area.y + area.height - EPICK_HEIGHT / 2, 2 * EPICK_WIDTH, EPICK_HEIGHT / 2);
+	Mat botRight = src(botRoi);
+	float botScore = countNonZero(botRight) / (float) (botRight.rows * botRight.cols);
+	if (botScore < 0.3 && area.height < 3.5 * EPICK_HEIGHT) {
+		std::cout << "form 2" << std::endl;
+		return FORM2;
+	}
+	Rect topRoi(area.x, area.y, 4 * EPICK_WIDTH, EPICK_HEIGHT / 2);
+	Mat topLeft = src(topRoi);
+	float topScore = countNonZero(topLeft) / (float) (topLeft.rows * topLeft.cols);
+
+	Rect midRoi(area.x + area.width - 2 * EPICK_WIDTH, area.y + 1.25 * EPICK_HEIGHT, 2 * EPICK_WIDTH, EPICK_HEIGHT / 2);
+	Mat midRight = src(midRoi);
+	float midScore = countNonZero(midRight) / (float) (midRight.rows * midRight.cols);
+	form2Score = (form2Score + (3 - topScore - botScore - midScore) / 3) / 2;
+	if (form2Score > 0.7) {
+		std::cout << "form 3" << std::endl;
+		return FORM3;
+	}
+	return FORM1;
+}
+
+void findForm2Areas(cv::Mat& src, cv::Rect& area, vector<Rect>& rects) {
+	Rect roi = area;
+	roi.width = roi.width -  2.2 * EPICK_WIDTH;
+	Mat temp = src(roi);
+	Rect r = findBoundingRect(temp, src.rows / 7.0f, src.cols / 4.0f);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+
+	//top right
+	roi.height = 1.5 * EPICK_HEIGHT;
+	roi.y = roi.y - 1.25 * EPICK_HEIGHT;
+	roi.x += roi.width;
+	roi.width = 2.2 * EPICK_WIDTH;
+//	rectangle(src, roi, Scalar(255,255, 255)); //debug
+	temp = src(roi);
+	r = findBoundingRect(temp, 0.5 * EPICK_HEIGHT, EPICK_WIDTH);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+
+	roi.y = r.y + 2.3 * EPICK_HEIGHT;
+//	rectangle(src, roi, Scalar(255,255, 255)); //debug
+	temp = src(roi);
+	r = findBoundingRect(temp, 0.5 * EPICK_HEIGHT, EPICK_WIDTH);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+}
+
+void findForm3Areas(cv::Mat& src, cv::Rect& area, vector<Rect>& rects) {
+	Rect roi = area;
+	roi.width = roi.width -  2.2 * EPICK_WIDTH;
+	Mat temp = src(roi);
+	Rect r = findBoundingRect(temp, src.rows / 7.0f, src.cols / 4.0f);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+
+	//top right
+	roi.height = 1.5 * EPICK_HEIGHT;
+	roi.x += roi.width;
+	roi.width = 2.2 * EPICK_WIDTH;
+	temp = src(roi);
+	r = findBoundingRect(temp, 0.5 * EPICK_HEIGHT, EPICK_WIDTH);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+
+	roi.y += 2.3 * EPICK_HEIGHT;
+//	rectangle(src, roi, Scalar(255,255, 255)); //debug
+	temp = src(roi);
+	r = findBoundingRect(temp, 0.5 * EPICK_HEIGHT, EPICK_WIDTH);
+	if (r.width == 0 || r.height == 0) {
+		throw std::bad_exception();
+	}
+	r.x += roi.x;
+	r.y += roi.y;
+	rects.push_back(r);
+}
 
 void detectAreas(cv::Mat& src, vector<Rect>& rects) {
 	std::vector<int> projects;
@@ -138,23 +268,31 @@ void detectAreas(cv::Mat& src, vector<Rect>& rects) {
 	int endY = src.rows - 10;
 	for (; endY >= 0&& projects[endY] < thresY; --endY) {}
 
-	//debug draw
-	line( src, Point(startX, 0), Point(startX, src.rows), Scalar(255,255, 255), 1, CV_AA);
-	line( src, Point(endX, 0), Point(endX, src.rows), Scalar(255,255, 255), 1, CV_AA);
-	line( src, Point(0, startY), Point(src.cols, startY), Scalar(255,255, 255), 1, CV_AA);
-	line( src, Point(0, endY), Point(src.cols, endY), Scalar(255,255, 255), 1, CV_AA);
-
-	Rect roi(startX, startY, endX - startX, endY - startY);
-	Mat temp = src(roi);
 	if (endY - startY < src.rows * 0.28 || endX - startX < src.cols * 0.8) {
 		throw std::bad_exception();
-	} else if (endY - startY < 4 * EPICK_HEIGHT) {
-		//form 2 epick
-		std::cout << "form 2" << std::endl;
-	}
-	if (endY > EPICK_ON_BOTTOM) {
+	} else if (endY > EPICK_ON_BOTTOM) {
 		//option epick on bottom
 		std::cout <<"epick on bottom" << std::endl;
+		int startYBE = endY - 2 * EPICK_HEIGHT;
+		for (; startYBE < src.rows && projects[startYBE] < thresY; ++startYBE) {}
+		rects.push_back(Rect(startX, startYBE, endX- startX, endY - startYBE));
+		for (endY = endY - 2 * EPICK_HEIGHT; endY >= 0&& projects[endY] < thresY; --endY) {}
+		rects.push_back(Rect(startX, startY, endX- startX, endY - startY));
+		return;
+	}
+	//detect form 2
+	Rect area(startX, startY, endX- startX, endY - startY);
+	int type = detectForm(src, area, rects);
+	switch (type) {
+		case FORM1:
+			rects.push_back(area);
+			break;
+		case FORM2:
+			findForm2Areas(src, area, rects);
+			break;
+		case FORM3:
+			findForm3Areas(src, area, rects);
+			break;
 	}
 }
 
@@ -172,6 +310,12 @@ float UVAnalyzer::checkValid(cv::Mat& uvImg) {
 	vector<Rect> rects;
 	imshow("bi", bi);
 	detectAreas(bi, rects);
+	//debug print rects
+	for (Rect rect : rects) {
+		rectangle(uvImg, rect, Scalar(255,255, 255));
+	}
+	imshow("src", uvImg);
+	imshow("bi", bi);
 	return 1;
 }
 
